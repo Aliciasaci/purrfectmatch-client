1a9203cdba00b3e7af02959e612cea0285db9036
Alicia Saci <lyciiasci@gmail.com>
2024-04-09 14:02:12 +0200

    add bottom navigation bar

---
+++ lib/composants/bottom_navigation_bar.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  final int selectedIndex;
+  final ValueChanged<int>? onItemTapped;
+
+  const CustomBottomNavigationBar({
+    Key? key,
+    required this.selectedIndex,
+    this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.00),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+        gradient: LinearGradient(
+          colors: [Colors.white, Colors.grey.withOpacity(0.5)],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.search, 0, Colors.green),
+              label: 'Chercher',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.pets, 1, Colors.pink),
+              label: 'Adopter',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(
+                  Icons.supervised_user_circle, 2, Colors.amber),
+              label: 'Profil',
+            ),
+          ],
+          currentIndex: widget.selectedIndex,
+          onTap: widget.onItemTapped,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIcon(IconData iconData, int index, Color color) {
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Icon(
+            iconData,
+            color: color,
+          ),
+        ),
+        if (widget.selectedIndex == index)
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: color.withOpacity(0.1),
+              ),
+            ),
+          )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'composants/bottom_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'PurrfectMatch',
-      theme: ThemeData( scaffoldBackgroundColor: Colors.amberAccent[100]),
-      home: const MyHomePage(title: 'PurrfectMatch'),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.amber[100]),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -27,31 +28,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title,  textAlign: TextAlign.center),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage('assets/logo.png'),
+              height: 30,
+              width: 30,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              widget.title,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
         centerTitle: true,
       ),
       body: Center(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Calls',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Camera',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Chats',
-          ),
-        ],
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
