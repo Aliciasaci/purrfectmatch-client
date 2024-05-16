2f6d9e986b1ef0af087298b0d7817943a455babc
Alicia Saci <lyciiasci@gmail.com>
2024-05-16 23:48:42 +0200

    update create chat avec tout les attributs

--- lib/composants/form_add_cat.dart
+++ lib/composants/form_add_cat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:intl/intl.dart';
 
 class AddCat extends StatefulWidget {
   const AddCat({super.key});
@@ -9,8 +10,43 @@ class AddCat extends StatefulWidget {
 }
 
 class _AddCatState extends State<AddCat> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _birthDateController = TextEditingController();
+  final TextEditingController _lastVaccineDateController = TextEditingController();
+  final TextEditingController _lastVaccineNameController = TextEditingController();
+  final TextEditingController _colorController = TextEditingController();
+  final TextEditingController _behaviorController = TextEditingController();
+  final TextEditingController _raceController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
   String? _selectedValue;
+  bool _sterilized = false;
+  bool _reserved = false;
   final List<String> _options = ['male', 'femelle'];
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _birthDateController.dispose();
+    _lastVaccineDateController.dispose();
+    _lastVaccineNameController.dispose();
+    _colorController.dispose();
+    _behaviorController.dispose();
+    _raceController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectDate(BuildContext context, TextEditingController controller) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null && picked != DateTime.now()) {
+      controller.text = DateFormat('yyyy-MM-dd').format(picked);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +57,10 @@ class _AddCatState extends State<AddCat> {
           color: Colors.white,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(40),
-            child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: SingleChildScrollView(
+            child: Form(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -36,106 +72,23 @@ class _AddCatState extends State<AddCat> {
                     ),
                   ),
                   const SizedBox(height: 20),
-                  Form(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: "Nom",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        TextFormField(
-                          maxLines: 2,
-                          decoration: const InputDecoration(
-                            labelText: "Description",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        TextFormField(
-                          maxLines: 1,
-                          decoration: const InputDecoration(
-                            labelText: "Age",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        TextFormField(
-                          maxLines: 1,
-                          decoration: const InputDecoration(
-                            labelText: "Color",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        TextFormField(
-                          maxLines: 1,
-                          decoration: const InputDecoration(
-                            labelText: "Comportement",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        TextFormField(
-                          maxLines: 1,
-                          decoration: const InputDecoration(
-                            labelText: "race",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        DropdownButton<String>(
-                          value: _selectedValue,
-                          hint: const Text('Sélectionnez un genre'), // Texte affiché avant la sélection
-                          items: _options.map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              _selectedValue = newValue; // Mise à jour de la valeur sélectionnée
-                            });
-                          },
-                          isExpanded: true,
-                        ),
-                        const SizedBox(height: 15),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Action à effectuer lors du clic sur le bouton de sélection de fichier
-                            FilePickerResult? result = await FilePicker.platform.pickFiles(
-                              type: FileType.custom,
-                              allowedExtensions: ['jpg', 'png', 'doc'],
-                            );
-                            if (result != null) {
-                              PlatformFile file = result.files.first;
-                              // Vous pouvez utiliser file pour obtenir des informations sur le fichier sélectionné
-                              print('Nom du fichier: ${file.name}');
-                              print('Chemin du fichier: ${file.path}');
-                              print('Taille du fichier: ${file.size}');
-                              print('Extension du fichier: ${file.extension}');
-                            } else {
-                              // Gérer le cas où aucun fichier n'est sélectionné
-                              print('Aucun fichier sélectionné.');
-                            }
-                          },
-                          child: const Text('Sélectionner un fichier'),
-                        ),
-                        const SizedBox(height: 15),
-                        ElevatedButton(
-                          onPressed: () {
-                            // Action à effectuer lors de la soumission du formulaire
-                            print('Formulaire envoyé');
-                          },
-                          child: const Text('Envoyer'),
-                        ),
-                        const SizedBox(height: 15),
-                      ],
-                    ),
+                  buildTextFormField(_nameController, "Nom"),
+                  buildTextFormFieldWithDatepicker(_birthDateController, "Date de Naissance", context),
+                  buildTextFormFieldWithDatepicker(_lastVaccineDateController, "Date du dernier vaccin", context),
+                  buildTextFormField(_lastVaccineNameController, "Nom du dernier vaccin"),
+                  buildTextFormField(_colorController, "Couleur"),
+                  buildTextFormField(_behaviorController, "Comportement"),
+                  buildTextFormField(_raceController, "Race"),
+                  buildDescriptionFormField(_descriptionController),
+                  buildGenderDropdown(),
+                  buildSwitchTile("Stérilisé", _sterilized, (value) => setState(() => _sterilized = value)),
+                  buildSwitchTile("Réservé", _reserved, (value) => setState(() => _reserved = value)),
+                  const SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () => print('Formulaire envoyé'),
+                    child: const Text('Envoyer'),
                   ),
+                  const SizedBox(height: 15),
                 ],
               ),
             ),
@@ -144,4 +97,55 @@ class _AddCatState extends State<AddCat> {
       ),
     );
   }
+
+  Widget buildTextFormField(TextEditingController controller, String label) => TextFormField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      border: OutlineInputBorder(),
+    ),
+  );
+
+  Widget buildTextFormFieldWithDatepicker(TextEditingController controller, String label, BuildContext context) => TextFormField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      border: OutlineInputBorder(),
+      suffixIcon: Icon(Icons.calendar_today),
+    ),
+    readOnly: true,
+    onTap: () => _selectDate(context, controller),
+  );
+
+  Widget buildDescriptionFormField(TextEditingController controller) => TextFormField(
+    controller: controller,
+    maxLines: 3,
+    decoration: InputDecoration(
+      labelText: "Description",
+      border: OutlineInputBorder(),
+    ),
+  );
+
+  Widget buildGenderDropdown() => DropdownButton<String>(
+    value: _selectedValue,
+    hint: const Text('Sélectionnez un genre'),
+    items: _options.map((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      );
+    }).toList(),
+    onChanged: (String? newValue) {
+      setState(() {
+        _selectedValue = newValue;
+      });
+    },
+    isExpanded: true,
+  );
+
+  Widget buildSwitchTile(String title, bool value, ValueChanged<bool> onChanged) => SwitchListTile(
+    title: Text(title),
+    value: value,
+    onChanged: onChanged,
+  );
 }
